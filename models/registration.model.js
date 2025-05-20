ab4c0220b39019163a5df433ce07d3f75ef92d42
shivam jha <shivamjha1907@gmail.com>
2025-05-20 17:16:04 +0530

    recruitments apply

--- models/registration.model.js
+++ models/registration.model.js
@@ -4,9 +4,9 @@ const Schema = mongoose.Schema;
 
 const registrationSchema = new Schema(
   {
-    event: {
+    recruitment: {
       type: Schema.Types.ObjectId,
-      ref: "Event",
+      ref: "Recruitment",
       required: true,
     },
     name: {
@@ -19,16 +19,6 @@ const registrationSchema = new Schema(
       required: true,
       trim: true,
       lowercase: true,
-      //match: [
-       // /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-       // "Please enter a valid email address",
-      //],
-    },
-    phone: {
-      type: String,
-      required: true,
-      trim: true,
-      // match: [/^\d{10}$/, "Please enter a valid 10-digit phone number"],
     },
     registeredAt: {
       type: Date,
@@ -39,5 +29,4 @@ const registrationSchema = new Schema(
 );
 
 const Registration = mongoose.model("Registration", registrationSchema);
-
 export default Registration;
