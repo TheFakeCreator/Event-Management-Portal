18d0175b4527d5333597e928f5a2998f17bb49b9
TheFakeCreator <166818795+TheFakeCreator@users.noreply.github.com>
2025-05-21 16:39:22 +0530

    added edit route for event

--- routes/event.routes.js
+++ routes/event.routes.js
@@ -12,6 +12,8 @@ import {
   getEventDetails,
   getEventRegister,
   registerEvent,
+  getEditEvent,
+  editEvent,
 } from "../controllers/event.controller.js";
 
 const router = express.Router();
@@ -21,10 +23,12 @@ router.get("/", isAuthenticatedLineant, getEvents);
 router.get("/create", isAuthenticated, getCreateEvent);
 router.get("/:id", isAuthenticated, getEventDetails);
 router.get("/:id/register", isAuthenticated, getEventRegister);
+router.get("/:id/edit", isAuthenticated, getEditEvent);
 
 // POST Routes
 router.post("/create", isAuthenticated, createEvent);
 router.post("/:id/register", isAuthenticated, registerEvent);
 router.post("/:id/delete", isAuthenticated, deleteEvent);
+router.post("/:id/edit", isAuthenticated, editEvent);
 
 export default router;
